f92b308ac4f09bbdde3b06c4bbd5e1b09d482b66
ladifatunkendriyu <151394317+ladifatunkendriyu@users.noreply.github.com>
2024-05-02 20:29:04 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,1 @@
-*/
-terraform {
-  backend "s3" {
-    bucket = "fusisoft-terraform-backend-bucket"
-    key    = "infra_resources/terraform.tfstate"
-    region = "us-west-2"
-    dynamodb_table = "terraform-state-locking"
-  }
-}
-/*
+
